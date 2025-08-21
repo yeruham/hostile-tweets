c3e1ce2b9fe57597646d359efc9fb73fe6ef8e36
YeruhamMendelson <y0583207549@gmail.com>
2025-08-21 09:49:34 +0300

    skeleton of processor

---
+++ app/processor.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+class Processor:
+
+    def __init__(self, data_frame: pd.DataFrame):
+        self.df = data_frame
+
+
+    def add_rarest_word(self):
+        pass
+
+    def add_sentiment(self):
+        pass
+
+    def weapons_detected(self):
+        pass
+
